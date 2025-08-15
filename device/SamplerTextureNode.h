f14409f68a0835ea34e69f01cdf6b245b394c1f1
Jefferson Amstutz <jamstutz@nvidia.com>
2025-08-15 15:41:29 -0500

    WIP

---
+++ device/SamplerTextureNode.h
@@ -0,0 +1,21 @@
+// Copyright 2025 Jefferson Amstutz
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+// cycles
+#include "scene/shader_nodes.h"
+
+CCL_NAMESPACE_BEGIN
+
+struct SamplerTextureNode : public TextureNode
+{
+  SHADER_NODE_NO_CLONE_CLASS(SamplerTextureNode)
+  ShaderNode *clone(ShaderGraph *graph) const override;
+  ImageHandle handle;
+
+ private:
+  void cull_tiles(Scene *scene, ShaderGraph *graph);
+};
+
+CCL_NAMESPACE_END
